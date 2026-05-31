e6960e6177c810d1b08b3657905a60fde840e8cc
Dinaf Rakipov <rakipov69@gmail.com>
2026-05-31 20:29:09 +0000

    fix(nvim): image.nvim backend

--- dot_config/nvim/lua/plugins/diagram.lua
+++ dot_config/nvim/lua/plugins/diagram.lua
@@ -1,7 +1,9 @@
 return {
   "3rd/diagram.nvim",
   dependencies = {
-    { "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
+    { "3rd/image.nvim", opts = {
+      backend = "sixel",
+    } }, -- you'd probably want to configure image.nvim manually instead of doing this
   },
   opts = { -- you can just pass {}, defaults below
     events = {
