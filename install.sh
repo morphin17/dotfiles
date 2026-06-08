e08dcd05c2bfbd51f16661a06b47ae3e2f12251b
Dinaf Rakipov <rakipov69@gmail.com>
2026-06-08 21:25:16 +0500

    chore(script): add install sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+# tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# neovim plugins
+nvim --headless "+Lazy! sync" +qa
+nvim --headless "+TSUpdate" +qa
+nvim --headless "+MasonUpdate" +qa
